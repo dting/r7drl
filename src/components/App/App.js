7c164dd07c2ebc24dc2e05fce45980b511187e39
Dennis Ting <dennisting@gmail.com>
2017-02-02 00:23:54 -0800

    Move App to components directory

---
+++ src/components/App/App.js
@@ -0,0 +1,16 @@
+import React, { PureComponent } from 'react';
+import './App.scss';
+
+class App extends PureComponent {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to React</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
