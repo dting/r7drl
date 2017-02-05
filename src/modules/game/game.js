1d0c252b8e72edd2ebf6d0186b28a5c165618815
Dennis Ting <dennisting@gmail.com>
2017-02-04 20:50:59 -0800

    Add levels 1-4

--- src/modules/game/game.js
+++ src/modules/game/game.js
@@ -1,4 +1,23 @@
 export default class Game {
+  static LEVELS = {
+    level1: {
+      level: 1,
+      name: 'Crust',
+    },
+    level2: {
+      level: 2,
+      name: 'Mantle',
+    },
+    level3: {
+      level: 3,
+      name: 'Outer Core',
+    },
+    level4: {
+      level: 4,
+      name: 'Inner Core',
+    },
+  };
+
   constructor(display, height, width) {
     this.display = display;
     this.height = height;
@@ -7,10 +26,7 @@ export default class Game {
 
   init(state) {
     return {
-      level: {
-        level: 1,
-        name: 'Spooky Dungeon',
-      },
+      level: Game.LEVELS.level1,
       message: "You are exploring",
     };
   }
